8827bc547c2e9d047b9a12370de93161b4a16806
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-23 20:56:01 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -8,24 +8,35 @@ class PigLatinizer
   end
 
   def piglatinize
-    x = []
-    words.each do |word|
-      if word.start_with?("a", "e", "i", "o", "u")
-        x = word.split("").shift()
-        x << word.delete_prefix(x)+x+"ay"
-      else
-        x = word.split("").shift()
-        x << word.delete_prefix(x)+x+"way"
-      end
-    end
-    binding.pry
-    return x.join(" ")
+    x = "Once upon a time and a very good time it was there was a moocow coming down along the road and this moocow that was coming down along the road met a nice little boy named baby tuckoo"
+    x.split(" ").each do |word|
+      puts word
+      binding.pry
   end
+end
 
 =begin  def split_sentence
      split_it = @words.split(" ")
      x = []
      split_it.each do |word|
+
+
+     if word.start_with?("a", "e", "i", "o", "u")
+        sw = word.split("").shift()
+        x << word.delete_prefix(sw)+sw+"ay"
+      else
+        sw = word.split("").shift()
+        x << word.delete_prefix(sw)+sw+"way"
+      end
+      binding.pry
+    end
+    return x.join(" ")
+
+
+
+
+
+
 =end
 
 end
